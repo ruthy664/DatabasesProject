6946abf0c2dbe5ebd6363f1bb50a1f95227d319e
ebhermann <emmahermann764@gmail.com>
2026-05-12 12:33:21 -0400

    Update CustomerRestaurantSelectionUI.java

--- java_swing/CustomerRestaurantSelectionUI.java
+++ java_swing/CustomerRestaurantSelectionUI.java
@@ -21,7 +21,7 @@ public class CustomerRestaurantSelectionUI extends JFrame {
     // DATABSE INFORMATION, REVIEW BEFORE LAUNCHING!!!
     String url = "jdbc:mysql://localhost:3306/food_delivery";
     String user = "root";
-    String password = "Pleasework4!";
+    String password = "password";
 
     int customerID;
     JComboBox<String> restaurantDropdown;
